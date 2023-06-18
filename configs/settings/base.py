f84184971c1b6db0503aa9b8b01300c69853a22c
Harish Navnit <harishnavnit@gmail.com>
2023-06-18 21:44:57 +0530

    Move apps inside a subdir

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import sys
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
