fe78a41dfd55d860c46abb35eb1eba13adcd5a95
Harish Navnit <harishnavnit@gmail.com>
2023-06-18 17:50:39 +0530

    Store project configurations in a configs folder

--- configs/settings.py
+++ configs/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'azkaban.urls'
+ROOT_URLCONF = 'configs.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'azkaban.wsgi.application'
+WSGI_APPLICATION = 'configs.wsgi.application'
 
 
 # Database
