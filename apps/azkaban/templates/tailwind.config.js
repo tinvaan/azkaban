9012e658943cba6c90b3facdb875af0097da12ef
Harish Navnit <harishnavnit@gmail.com>
2023-06-19 23:13:39 +0530

    Setup tailwind project

--- apps/azkaban/templates/tailwind.config.js
+++ apps/azkaban/templates/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./azkaban/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
-
